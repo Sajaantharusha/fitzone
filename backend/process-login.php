e35762f780a29894b38490bbf2a80d9d5053889c
sajan tharusha <sajaantharush@gmail.com>
2025-05-08 16:01:59 +0530

    fix bugs

--- backend/process-login.php
+++ backend/process-login.php
@@ -22,13 +22,13 @@ if ($result->num_rows > 0) {
     }
 
     // User found, set session variables and redirect to dashboard
-    $_SESSION['user_id'] = $row['id'];
+    $_SESSION['user_id'] = $row['user_id'];
     $_SESSION['user_name'] = $row['user_name'];
     $_SESSION['role'] = $row['role'];
     if($row['role'] == 'admin') {
-        header("Location: ../admin.php");
+        header("Location: ../admin-dashboard.php");
     } else {
-        header("Location: ../#Services");
+        header("Location: ../user-dashboard.php");
     }
     
     exit();
